958ed6535fe0aa616327effa9267739f802115b5
Reid Poole <poole.reid@gmail.com>
2023-03-16 22:50:05 -0500

    Add New Stuff

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -28,17 +28,20 @@ const Homepage = () => {
         <p className="intro">Most importantly, I am a proud husband and father of 2!</p>
         <p className="intro">...Come on in!</p>
       </article>
+
+
       <section id="aboutMe" className="about-me">
         <article className="read-about">
+          <h3>Software Development Experience</h3>
           <p className="about-me-text">
             My expertise lies within Javascript, React.js, Router, HTML, and CSS. I also have Backend experience with building full-stack apps using Express.js, Knex.js, and PostgreSQL.
-
- I am a through-and-through musician that has a deep connection and experience in music. I've been performing full-time in New Orleans as well as on national and international tours for the past 13 years. I relate the language of music to code in many substantial ways that carry over very easily and intuitively. 
-
-I am an Educator that has taught for more than 15 years to a spectrum of ages, from middle school to high school, and private lesson studios, but I'd like to highlight my experience in Higher Education as a professor of Music Industry at Dillard University. 
-
-Finally, I'd like to share that I have experience in Tech and Software from being a digital archivist for the Louis Armstrong House Museum, based in Queens, NY, helping to develop the rich history of Louis Armstrong for easy user and fan access online. I'm especially proud of this because it includes my three passions of Software Development, Music, and Education!
-          </p>
+            </p>
+            <p className="about-me-text">I am a through-and-through musician that has a deep connection and experience in music. I've been performing full-time in New Orleans as well as on national and international tours for the past 13 years. I relate the language of music to code in many substantial ways that carry over very easily and intuitively. 
+            </p>
+            <p className="about-me-text">I am an Educator that has taught for more than 15 years to a spectrum of ages, from middle school to high school, and private lesson studios, but I'd like to highlight my experience in Higher Education as a professor of Music Industry at Dillard University. 
+            </p>
+            <p className="about-me-text">Finally, I'd like to share that I have experience in Tech and Software from being a digital archivist for the Louis Armstrong House Museum, based in Queens, NY, helping to develop the rich history of Louis Armstrong for easy user and fan access online. I'm especially proud of this because it includes my three passions of Software Development, Music, and Education!
+            </p>
         </article>
       </section>
       <section id="work" className="work-container">
@@ -47,7 +50,11 @@ Finally, I'd like to share that I have experience in Tech and Software from bein
           <section className="project-info">
             <iframe src={artistArchiveGif2} title="artistArchive" frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
             <section className="description-buttons">
-              <article className="proj-description">Artist Archive aims to inspire the uninspired ear!  This website contains a special collection of Incredible musical artist from many differing genres.  Look through the Archive and find new and exciting musical artists!  Each artist click takes you to a description and an inspiring musical video of the Artist.  But wait, if a user finds an artists that resonate with them, you can collect your favorites into a private favorites collections!  Don't waste another moment and find the deployed site below!!</article>
+              <article className="proj-description">
+                <p className="description-text">
+                  Artist Archive aims to inspire the uninspired ear!  This website contains a special collection of Incredible musical artist from many differing genres.  Look through the Archive and find new and exciting musical artists!  Each artist click takes you to a description and an inspiring musical video of the Artist.  But wait, if a user finds an artists that resonate with them, you can collect your favorites into a private favorites collections!  Don't waste another moment and find the deployed site below!!
+                </p>
+                  </article>
               <a href="https://github.com/rpoole444/ArtistArchive-FE">
                 <button className="proj-buttons">Repo</button>
               </a>
@@ -62,7 +69,11 @@ Finally, I'd like to share that I have experience in Tech and Software from bein
           <section className="project-info">
             <iframe src="https://giphy.com/embed/yKym67WoKt3AIQ38UH"  title="shelfLife" frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
             <section>
-              <article className="proj-description">Do you have a hard time picking out the right book to read? Well look no further, Shelf Life is here to help! This App is a book recomendation app where you are able to look at a database of books referred by the Creators and add the book to your favorites based on your interest and the recomendation! Further more, you are able to search the New York Times top 100 best sellers and add your recommendation to the recommendation database.</article>
+              <article className="proj-description">
+                <p className="description-text">
+                  Do you have a hard time picking out the right book to read? Well look no further, Shelf Life is here to help! This App is a book recomendation app where you are able to look at a database of books referred by the Creators and add the book to your favorites based on your interest and the recomendation! Further more, you are able to search the New York Times top 100 best sellers and add your recommendation to the recommendation database.
+                </p>
+                </article>
               <a href="https://github.com/rpoole444/shelf-life-FE">
                 <button className="proj-buttons">Repo</button>
               </a>
@@ -73,28 +84,36 @@ Finally, I'd like to share that I have experience in Tech and Software from bein
           </section>
         </section>
         <section className="project-container">
-          <h2 className="proj-title">Fit Lit</h2>
-          <section className="project-info">
-            <iframe src={fitLitGif} title="fitLit" frameBorder="0" className="giphy-embed fit-lit"></iframe>
-            <section>
-              <article className="proj-description">The purpose of this app is to display a user's data, such as sleep, hydration and activity for the latest day and week including comparisions to the average of all user's for each topic. The user is able to add their own data for a given date.</article>
-              <a href="https://github.com/jheidepriem/fitLit-tracker">
-                <button className="proj-buttons">Repo</button>
-              </a>
-            </section>
-          </section>
-        </section>
-        <section className="project-container">
           <h2 className="proj-title">Ceaseless Cinemas</h2>
           <section className="project-info">
             <iframe src={ceaselessCinemaGif} title="ceaselessCinema" frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
             <section>
-              <article className="proj-description">If you're interested in the latest films, Ceaseless Cinema is here for you! You can search through all of the films and find their trailers and film details such as, runtime, release date, budge, revenue, description, and their official trailer.</article>
+              <article className="proj-description">
+                <p className="description-text">
+                If you're interested in the latest films, Ceaseless Cinema is here for you! You can search through all of the films and find their trailers and film details such as, runtime, release date, budge, revenue, description, and their official trailer.
+                </p>
+                </article>
               <a href="https://github.com/rpoole444/rancidTomatillos">
                 <button className="proj-buttons">Repo</button>
               </a>
               <a href="https://rancid-tomatillos-tau.vercel.app/">
                 <button className="proj-buttons">Visit Site</button>
+              </a>
+            </section>
+          </section>
+        </section>
+        <section className="project-container">
+          <h2 className="proj-title">Fit Lit</h2>
+          <section className="project-info">
+            <iframe src={fitLitGif} title="fitLit" frameBorder="0" className="giphy-embed fit-lit"></iframe>
+            <section>
+              <article className="proj-description">
+                <p className="description-text">
+                The purpose of this app is to display a user's data, such as sleep, hydration and activity for the latest day and week including comparisions to the average of all user's for each topic. The user is able to add their own data for a given date.
+                </p>
+                </article>
+              <a href="https://github.com/jheidepriem/fitLit-tracker">
+                <button className="proj-buttons">Repo</button>
               </a>
             </section>
           </section>
