95c1322e4198e8baf2a2dd8647eb6e18a10c2a08
Reid Poole <poole.reid@gmail.com>
2023-03-20 10:54:55 -0500

    Edit text

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -13,7 +13,7 @@ const Homepage = () => {
       <article className="introduction-container">
         <h2 className="intro-title">Thanks for dropping by!</h2>
         <p className="intro">Allow Me to Introduce myself!</p>
-        <p className="intro">I'm Reid Poole 👋, and I'm a Software Developer</p>
+        <p className="intro">I'm Reid Poole 👋</p>
         <p className="intro">
           This site is dedicated to my work as a Software Engineer and
           Developer!
