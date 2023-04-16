292b210462f1e5daef4af52c5f3f74aecee54c68
Reid Poole <poole.reid@gmail.com>
2023-04-16 10:42:45 -0500

    minor change

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -9,8 +9,7 @@ const Homepage = () => {
     <main className="homepage">
       <h1 className="homepage-title">Welcome To Reid Poole's Portfolio</h1>
       <article className="introduction-container">
-        <h2 className="intro-title">Thanks for dropping by!</h2>
-        <p className="intro">Allow Me to Introduce myself!</p>
+        <p className="intro-title">Allow Me to Introduce myself!</p>
         <p className="intro">I'm Reid Poole 👋</p>
         <article className="intro">
           This site is dedicated to my work as a Software Engineer and
