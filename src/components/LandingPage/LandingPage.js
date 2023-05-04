3e743340c58bb43f823e5618ba8307fec34df248
Reid Poole <poole.reid@gmail.com>
2023-05-04 09:12:59 -0500

    Add more detail to landing page WIP

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -3,19 +3,13 @@ import "./LandingPage.css";
 import logo2 from "../assets/insta.png";
 import logo3 from "../assets/github-mark.png";
 import logo4 from "../assets/linked.png";
-import emailImg from "../assets/email.png";
+import trumpet1 from "../assets/TrumpetReid1.jpg";
 import Header from "../Header/Header";
 
 const LandingPage = () => {
   return (
     <div className="entrance-container">
       <aside className="aside">
-        <section className="icon-skillz-container">
-          <i className="devicon-typescript-plain colored skillz"></i>
-          <i className="devicon-react-original-wordmark colored skillz"></i>
-          <i className="devicon-express-original-wordmark skillz"></i>
-          <i className="devicon-postgresql-plain-wordmark skillz"></i>
-        </section>
         <section className="social-logos">
           <a
             href="https://www.instagram.com/reid_poole_music/"
@@ -39,8 +33,20 @@ const LandingPage = () => {
             <img className="logo-side" src={logo4} alt="linked-in-logo" />
           </a>
         </section>
+        <section className="icon-skillz-container">
+          <i className="devicon-typescript-plain colored skillz"></i>
+          <i className="devicon-react-original-wordmark colored skillz"></i>
+          <i className="devicon-express-original-wordmark skillz"></i>
+          <i className="devicon-postgresql-plain-wordmark skillz"></i>
+        </section>
       </aside>
-      <img src={emailImg} className="email-icon" />
+      <section className="main-section">
+        <section className="info-section">
+          <h1 className="main-title"> Reid Poole - Software Engineer</h1>
+          <h2 className="sub-main-title"> Musician | Educator | Father | Husband</h2>
+        </section>
+        <img className="reid-photo"src={trumpet1} alt="Reid Playing Trumpet" />
+      </section>
     </div>
   );
 };
